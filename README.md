9b7a0ec7474b99f8186fac5aa13ce83103065c56
AshineFoster <ashinefoster@gmail.com>
2021-10-17 11:02:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Sieve of Eratosthenes
+
+Finds all prime numbers less than 1,000,000 using the sieve of Eratosthenes.
+
+## Preview
+
+![Primes file](prime1.png)
+![Primes file-2](prime2.png)
+
+## Installation
+
+If you already have a recent version of Rust installed just clone this repo.
+
+## Usage
+
+Open your terminal in the project directory and then run the command:
+
+```sh
+cargo run
+```
+
+## License
+
+[GNU General Public License version 3](https://opensource.org/licenses/GPL-3.0)
+
+## Leave a Star
+
+Leave a star in GitHub.
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+const NUMTO10: [u8; 10] = [0, 1, 1, 0, 1, 0, 1, 0, 0, 0];
+const LIMIT: usize = 1_000_000;
+
+fn main() {
+    // create a array of integers
+    let mut values: [u8; LIMIT] = [1; LIMIT];
+    // set value as composite if it is a multiple of another number
+    set_composite(&mut values);
+    // gets the prime numbers
+    let primes = get_primes(&values);
+    // print values that are prime
+    println!("Prime numbers:");
+    println!("{:?}", primes);
+}
+
+/// Given an array of values returns an array with the prime numbers set to 1 and composite to 0.
+fn set_composite(values: &mut [u8]) -> &[u8] {
+    values[0] = 0;
+    let sqr_val_len = (values.len() as f64).sqrt() as usize;
+
+    for index in 2..=sqr_val_len {
+        if values[index - 1] == 1 {
+            let mut multiples = index * index;
+            while multiples <= values.len() {
+                values[multiples - 1] = 0;
+                multiples += index;
+            }
+        }
+    }
+    values
+}
+
+#[test]
+fn test_set_composite() {
+    let mut test_array: [u8; 100] = [1; 100];
+    assert_eq!(set_composite(&mut test_array[0..10]), NUMTO10);
+}
+
+/// returns a vector of the prime numbers in the given array
+fn get_primes(values: &[u8]) -> Vec<usize> {
+    let mut primes = Vec::new();
+
+    for (index, val) in values.iter().enumerate() {
+        if *val == 1 {
+            primes.push(index + 1)
+        }
+    }
+    primes
+}
+
+#[test]
+fn test_get_primes() {
+    assert_eq!(get_primes(&NUMTO10), vec![2, 3, 5, 7]);
+}
